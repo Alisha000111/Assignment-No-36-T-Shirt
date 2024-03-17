118a24dd174e095eef3c359c05386d9b9852e1ea
Alisha M Irfan <160208713+Alisha000111@users.noreply.github.com>
2024-03-17 17:45:51 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+// Write a function called make_shirt() that accepts a size and the text of a message that should be printed on the shirt. The function should print a sentence summarizing the size of the shirt and the message printed on it. Call the function.
+function makeShirt(size, text) {
+    console.log("you order a ".concat(size, " shirt that says ").concat(text));
+}
+makeShirt('large', '" i love typescript"');
+makeShirt('medium', '" i need a big shirt"');
---
+++ app.ts
@@ -0,0 +1,7 @@
+// Write a function called make_shirt() that accepts a size and the text of a message that should be printed on the shirt. The function should print a sentence summarizing the size of the shirt and the message printed on it. Call the function.
+function makeShirt(size: string, text: string): void {
+    console.log(`you order a ${size} shirt that says ${text}`)
+}
+makeShirt('large','" i love typescript"')
+makeShirt('medium','" i need a big shirt"')
+
